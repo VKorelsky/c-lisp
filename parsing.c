60c6cf07b6092c9da65a4c8f8ffb9bfb596d311b
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-08-11 14:54:45 +0100

    refacto

--- parsing.c
+++ parsing.c
@@ -3,7 +3,42 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
+void init_mpc() {
+  /* mpc logic */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Kispy    = mpc_new("kispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                   \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    kispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Kispy);
+
+    mpc_cleanup(4, Number, Operator, Expr, Kispy);
+}
+
+void mpc_parse(){
+  /* parse user input */
+  mpc_result_t r;
+
+  if(mpc_parse("<stdin>", input, Lispy, &r) {
+    /* success */
+    mpc_ast_print(r.output);
+    mpc_ast_delete(r.output);
+  } else {
+    /* error */
+    mpc_ast_print(r.error);
+    mpc_ast_delete(r.error);
+  }
+}
+
 int main(int argc, char** argv) {
+  init_mpc();
 
   puts("Kisp Version 0.0.0.1");
   puts("Press Ctrl+c to exit");
